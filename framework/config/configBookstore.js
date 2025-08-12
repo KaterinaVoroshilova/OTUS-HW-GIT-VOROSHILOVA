fdc8ae5c3406778b181e882155898307e98539d7
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2025-08-12 15:38:01 +0300

    HW8

---
+++ framework/config/configBookstore.js
@@ -0,0 +1,7 @@
+export default Object.freeze({
+  baseURL: 'https://bookstore.demoqa.com',
+  userName: `user`,
+  password: `Pass!123`,
+  uncorrectPass: `pass`,
+  userId: `a067f59e-0b01-48e5-801c-8e23fe848768`
+})
---
+++ framework/services/UserService.js
@@ -0,0 +1,51 @@
+import config from '../config/configBookstore'
+
+const getUser = async (userId, token) => {
+  const response = await fetch(`${config.baseURL}/Account/v1/User/${userId}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  return {
+    headers: response.headers,
+    status: response.status,
+    data: response.data
+  }
+}
+
+const createUser = async (userName, password) => {
+  const response = await fetch(`${config.baseURL}/Account/v1/User`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ userName, password })
+  })
+
+  return {
+    headers: response.headers,
+    status: response.status,
+    data: await response.json()
+  }
+}
+
+const removeUser = async (userId, token) => {
+  const response = await fetch(`${config.baseURL}/Account/v1/User/${userId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  return {
+    headers: response.headers,
+    status: response.status,
+    data: response.data
+  }
+}
+
+export default {
+  get: getUser,
+  create: createUser,
+  remove: removeUser
+}
