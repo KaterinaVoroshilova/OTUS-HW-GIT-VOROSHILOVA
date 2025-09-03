6b55962f1ed6ab0b4633506be577728090b6f662
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2025-09-03 20:44:55 +0300

    hw docker begin

--- test/books.spec.ts
+++ test/books.spec.ts
@@ -6,7 +6,6 @@ import { books } from '../framework/fixtures/Books.json'
 //import configBookstore from '../framework/config/configBookstore'
 import { generateUserCredentials } from '../framework/fixtures/userFixture.ts'
 
-// @ts-expect-error TS(2593): Cannot find name 'test'. Do you need to install ty... Remove this comment to see the full error message
 test(`Add book`, async () => {
   const newUser = generateUserCredentials()
   const responseCreate = await UserService.create(newUser.userName, newUser.password)
@@ -22,11 +21,9 @@ test(`Add book`, async () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment 
   // @ts-expect-error 
   const responseAddListOfBooks = await BookService.addList(userId, collectionOfIsbns, token)
-  // @ts-expect-error TS(2304): Cannot find name 'expect'.
   expect(responseAddListOfBooks.data.books).toEqual(collectionOfIsbns)
 })
 
-// @ts-expect-error TS(2593): Cannot find name 'test'. Do you need to install ty... Remove this comment to see the full error message
 test(`Replace book`, async () => {
   const newUser = generateUserCredentials()
   const responseCreate = await UserService.create(newUser.userName, newUser.password)
@@ -46,7 +43,6 @@ test(`Replace book`, async () => {
   // @ts-expect-error 
   await BookService.addList(userId, collectionOfIsbns, token)
   const responseReplaceBook = await BookService.replace(userId, isbn1, isbn2, token)
-  // @ts-expect-error TS(2304): Cannot find name 'expect'.
   expect(responseReplaceBook.data).toEqual({
     books: [book2],
     userId,
@@ -54,20 +50,16 @@ test(`Replace book`, async () => {
   })
 })
 
-// @ts-expect-error TS(2593): Cannot find name 'test'. Do you need to install ty... Remove this comment to see the full error message
 test(`Get information about book`, async () => {
   const [book1] = books
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment 
   // @ts-expect-error 
   const isbn1 = book1.isbn
   const responseGetBook = await BookService.getBook(isbn1)
-  // @ts-expect-error TS(2304): Cannot find name 'expect'.
   expect(responseGetBook.data.isbn).toBe(isbn1)
-  // @ts-expect-error TS(2304): Cannot find name 'expect'.
   expect(responseGetBook.data.title).toEqual('Git Pocket Guide')
 })
 
-// @ts-expect-error TS(2593): Cannot find name 'test'. Do you need to install ty... Remove this comment to see the full error message
 test(`Delete book`, async () => {
   const newUser = generateUserCredentials()
   const responseCreate = await UserService.create(newUser.userName, newUser.password)
@@ -84,9 +76,7 @@ test(`Delete book`, async () => {
   // @ts-expect-error 
   await BookService.addList(userId, collectionOfIsbns, token)
   const responseRemove = await BookService.removeBooks(userId, isbn1, token)
-  // @ts-expect-error TS(2304): Cannot find name 'expect'.
   expect(responseRemove.status).toBe(204)
   const responseUser = await UserService.get(userId, token)
-  // @ts-expect-error TS(2304): Cannot find name 'expect'.
   expect(responseUser.data).toHaveProperty(`books`, [])
 })
