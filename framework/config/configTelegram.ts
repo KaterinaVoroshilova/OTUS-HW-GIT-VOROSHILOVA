cab73c121a4c3161d8a9b3cf3269d33f42a6e93b
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2025-09-06 02:09:06 +0300

    hw_fixed

--- framework/config/configTelegram.ts
+++
@@ -1,5 +0,0 @@
-export default Object.freeze({
-  enable: process.env.TELEGRAM_ENABLE === 'true',
-  token: process.env.TELEGRAM_TOKEN as string,
-  chatId: process.env.TELEGRAM_CHAT_ID as string
-})
